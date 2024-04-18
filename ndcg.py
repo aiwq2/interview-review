e6aaf9ad2e36abff92203f5fa391d9761e5e2ebf
aiwq2 <1340282915@qq.com>
2024-04-18 19:11:54 +0800

    添加ndcg

---
+++ ndcg.py
@@ -0,0 +1,7 @@
+r=np.array([1,0,0,1,0])
+dcg = r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+r_s = sorted(r,reverse=True)
+r_s = np.array(r_s)
+dcg_max = r_s[0] + np.sum(r_s[1:] / np.log2(np.arange(2, r_s.size + 1)))
+print(dcg,dcg_max)
+print(dcg/dcg_max)
